00f32659516bc1dcf52af28ddc7035404e6018de
Torjus Iveland <torjusti@gmail.com>
2016-12-28 23:54:57 +0100

    Validate table HTML

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -5,21 +5,25 @@ import { cubingAverage } from '../utils/cubingStatistics';
 const ResultList = ({ results, onDelete, onTogglePlusTwo, onToggleDNF }) => (
   <table className="ResultList">
     <thead>
-      <th>Time</th>
-      <th>ao5</th>
-      <th>ao12</th>
-      <th colSpan="3">Tools</th>
+      <tr>
+        <th>Time</th>
+        <th>ao5</th>
+        <th>ao12</th>
+        <th colSpan="3">Tools</th>
+      </tr>
     </thead>
-    {results.map((result, index) =>
-      <Result key={result.id}
-        onDelete={() => onDelete(result.session, result.id)}
-        onTogglePlusTwo={() => onTogglePlusTwo(result.session, result.id)}
-        onToggleDNF={() => onToggleDNF(result.session, result.id)}
-        currentAo5={index >= 4 && cubingAverage(results.slice(index - 4, index + 1))}
-        currentAo12={index >= 11 && cubingAverage(results.slice(index - 11, index + 1))}
-        {...result}
-      />
-    )}
+    <tbody>
+      {results.map((result, index) =>
+        <Result key={result.id}
+          onDelete={() => onDelete(result.session, result.id)}
+          onTogglePlusTwo={() => onTogglePlusTwo(result.session, result.id)}
+          onToggleDNF={() => onToggleDNF(result.session, result.id)}
+          currentAo5={index >= 4 && cubingAverage(results.slice(index - 4, index + 1))}
+          currentAo12={index >= 11 && cubingAverage(results.slice(index - 11, index + 1))}
+          {...result}
+        />
+      )}
+    </tbody>
   </table>
 );
 
