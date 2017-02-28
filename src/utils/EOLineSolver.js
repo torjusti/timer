aee5a4fe9ec29615955bc0b341209450b795edb8
Torjus Iveland <torjusti@gmail.com>
2017-02-28 14:27:30 +0100

    Solver performance improvements

--- src/utils/EOLineSolver.js
+++ src/utils/EOLineSolver.js
@@ -9,33 +9,21 @@ const EOLineSolver = scramble => {
 
   // Define moves.
   const moves =  [{
-    cp: [UBR, URF, UFL, ULB, DFR, DLF, DBL, DRB],
-    co: [0, 0, 0, 0, 0, 0, 0, 0],
     ep: [UB, UR, UF, UL, DR, DF, DL, DB, FR, FL, BL, BR],
     eo: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   }, {
-    cp: [DFR, UFL, ULB, URF, DRB, DLF, DBL, UBR],
-    co: [2, 0, 0, 1, 1, 0, 0, 2],
     ep: [FR, UF, UL, UB, BR, DF, DL, DB, DR, FL, BL, UR],
     eo: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   }, {
-    cp: [UFL, DLF, ULB, UBR, URF, DFR, DBL, DRB],
-    co: [1, 2, 0, 0, 2, 1, 0, 0],
     ep: [UR, FL, UL, UB, DR, FR, DL, DB, UF, DF, BL, BR],
     eo: [0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0],
   }, {
-    cp: [URF, UFL, ULB, UBR, DLF, DBL, DRB, DFR],
-    co: [0, 0, 0, 0, 0, 0, 0, 0],
     ep: [UR, UF, UL, UB, DF, DL, DB, DR, FR, FL, BL, BR],
     eo: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   }, {
-    cp: [URF, ULB, DBL, UBR, DFR, UFL, DLF, DRB],
-    co: [0, 1, 2, 0, 0, 2, 1, 0],
     ep: [UR, UF, BL, UB, DR, DF, FL, DB, FR, UL, DL, BR],
     eo: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   }, {
-    cp: [URF, UFL, UBR, DRB, DFR, DLF, ULB, DBL],
-    co: [0, 0, 1, 2, 0, 0, 2, 1],
     ep: [UR, UF, UL, BR, DR, DF, DL, BL, FR, FL, UB, DB],
     eo: [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1],
   }];
@@ -61,28 +49,12 @@ const EOLineSolver = scramble => {
 
   // The solved cube.
   const identityCube = {
-    cp: [0, 1, 2, 3, 4, 5, 6, 7],
-    co: [0, 0, 0, 0, 0, 0, 0, 0],
     ep: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
     eo: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   };
 
-  // Multiply cubes with respect to corners.
-  const cornerMultiply = (position, other) => {
-    const cp = [];
-    const co = [];
-
-    for (let i = 0; i < 8; i += 1) {
-      const from = other.cp[i];
-      cp[i] = position.cp[from];
-      co[i] = (position.co[from] + other.co[i]) % 3;
-    }
-
-    return Object.assign({}, position, { cp, co });
-  };
-
   // Multiply cubes with respect to edges.
-  const edgeMultiply = (position, other) => {
+  const multiply = (position, other) => {
     const ep = [];
     const eo = [];
 
@@ -93,11 +65,6 @@ const EOLineSolver = scramble => {
     }
 
     return Object.assign({}, position, { ep, eo });
-  };
-
-  const multiply = (position, other) => {
-    // EOLine only requires multiplicatino with respect to edges.
-    return edgeMultiply(position, other);
   };
 
   const parseAlg = (alg) => {
@@ -130,21 +97,20 @@ const EOLineSolver = scramble => {
     faceNames[num / 3 | 0] + ['', '2', '\''][num % 3]
   ).join(' ');
 
-  const inverseFormatSequence = sequence => sequence.reverse().map(num =>
-    faceNames[num / 3 | 0] + ['\'', '2', ''][num % 3]
-  ).join(' ');
+  const solved = ({ ep, eo }) => eo.every(o => o === 0) && ep[5] === 5 && ep[7] === 7;
 
-  const combineSequenceAndInverse = (seq, inv) => formatSequence(seq) + ' ' + inverseFormatSequence(inv);
+  const StateRepresentation = ({ ep, eo }) => {
+    let sum = 0;
 
-  const solved = ({ cp, co, ep, eo }) => eo.every(o => o === 0) && ep[5] === 5 && ep[7] === 7;
+    eo.forEach(edge => {
+      sum = 2 * sum + edge;
+    });
 
-  const StateRepresentation = ({ cp, co, ep, eo }) => {
-    return [eo.toString(), ep[5], ep[7]].join();
+    sum = sum * 2 + ep[5];
+    sum = sum * 2 + ep[7];
+
+    return sum;
   };
-
-  let foundInPruningTable = false;
-
-  let maxDepth = 0;
 
   const TreeSearch = position => { 
     const queue = [];
@@ -152,12 +118,11 @@ const EOLineSolver = scramble => {
 
     const states = {};
 
+    let best = Infinity;
+
     while (true) {
       const sequence = queue.shift() || [];
       const identitySequence = identityQueue.shift() || [];
-
-      let depth = identitySequence.length;
-      maxDepth = Math.max(maxDepth, depth);
 
       const sequenceResult = result(position, sequence);
       const identityResult = result(identityCube, identitySequence);
@@ -166,12 +131,18 @@ const EOLineSolver = scramble => {
         return formatSequence(sequence);
       }
 
-      states[StateRepresentation(identityResult)] = states[StateRepresentation(identityResult)] || identitySequence;
+      if (!states[StateRepresentation(identityResult)]) {
+        states[StateRepresentation(identityResult)] = identitySequence.length;
+      }
 
-      const storedSequence = states[StateRepresentation(sequenceResult)];
+      const stored = states[StateRepresentation(sequenceResult)];
 
-      if (storedSequence && storedSequence[0] / 3 | 0 !== sequence.slice(-1)[0] / 3 | 0) {
-        return combineSequenceAndInverse(sequence, storedSequence);
+      if (stored && stored < best) {
+        best = stored;
+      }
+
+      if (isFinite(best) && (!stored || stored > best)) {
+        continue;
       }
 
       for (let i = 0; i < 6; i++) {
@@ -181,16 +152,24 @@ const EOLineSolver = scramble => {
           continue;
         }
 
+        if (last === i - 3 && i % 2 === 0) {
+          continue;
+        }
+
         for (let j = 0; j < 3; j++) {
           queue.push(sequence.concat([i * 3 + j]));
         }
       }
 
-      if (depth >= maxDepth) {
+      if (!isFinite(best)) {
         for (let i = 0; i < 6; i++) {
           const last = (identitySequence.slice(-1)[0] / 3 | 0);
 
           if (identitySequence.length !== 0 && i === last) {
+            continue;
+          }
+
+          if (last === i - 3 && i % 2 === 0) {
             continue;
           }
 
