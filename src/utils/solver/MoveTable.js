5c2900eddfee6a3c352007a98522c3f6e1edc1b6
Torjus Iveland <torjusti@gmail.com>
2017-06-13 16:15:11 +0200

    Optimize move table initialization

--- src/utils/solver/MoveTable.js
+++ src/utils/solver/MoveTable.js
@@ -8,9 +8,30 @@ class MoveTable {
 
     for (let i = 0; i < size; i++) {
       this.table.push([]);
+    }
 
+    // For each index, loop through all moves and compute the
+    // resulting index after applying the move, and also map the
+    // inverse move back to the inital index.
+    for (let i = 0; i < size; i++) {
       for (let move = 0; move < 18; move++) {
-        this.table[i].push(doMove(i, move));
+        if (!this.table[i][move]) {
+          const result = doMove(i, move);
+
+          this.table[i][move] = result;
+
+          const pow = move % 3;
+
+          let inverse = move - pow;
+
+          if (pow === 0) {
+            inverse += 2;
+          } else if (pow === 1) {
+            inverse += 1;
+          }
+
+          this.table[result][inverse] = i;
+        }
       }
     }
   }
