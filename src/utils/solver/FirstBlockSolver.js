50828bb38b65614f3431419864ba33f1549da536
Torjus Iveland <torjusti@gmail.com>
2017-06-27 19:04:26 +0200

    Better pruning tables

--- src/utils/solver/FirstBlockSolver.js
+++ src/utils/solver/FirstBlockSolver.js
@@ -2,11 +2,15 @@ import Search from './Search';
 
 class FirstBlockSearch extends Search {
   initialize() {
-    this.addSimpleEdgeOrientationTable([6, 9, 10]);
-    this.addSimpleCornerOrientationTable([5, 6]);
-    
+    const EOMoves = this.addSimpleEdgeOrientationTable([6, 9, 10], true);
+    const COMoves = this.addSimpleCornerOrientationTable([5, 6], true);
+
     const EPMoves = this.addSimpleEdgePermutationTable([6, 9, 10], true);
     const CPMoves = this.addSimpleCornerPermutationTable([5, 6], true);
+
+    this.addPruningTable([EOMoves, CPMoves]);
+
+    this.addPruningTable([COMoves, CPMoves]);
 
     this.addPruningTable([EPMoves, CPMoves]);
   }
--- src/utils/solver/XCrossSolver.js
+++ src/utils/solver/XCrossSolver.js
@@ -2,13 +2,15 @@ import Search from './Search';
 
 class XCrossSearcher extends Search {
   initialize() {
-    this.addSimpleEdgeOrientationTable([4, 5, 6, 7, 8]);
-    this.addSimpleEdgePermutationTable([4, 5, 6, 7, 8]);
+    const EOMoves = this.addSimpleEdgeOrientationTable([4, 5, 6, 7, 8], true);
+    const EPMoves = this.addSimpleEdgePermutationTable([4, 5, 6, 7, 8], true);
 
     const COMoves = this.addSimpleCornerOrientationTable([4], true);
     const CPMoves = this.addSimpleCornerPermutationTable([4], true);
 
+    this.addPruningTable([EOMoves, CPMoves]);
     this.addPruningTable([COMoves, CPMoves]);
+    this.addPruningTable([EPMoves]);
   }
 }
 
