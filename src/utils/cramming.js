366855e596a5436dddbdd60474acadf2880218fa
Torjus Iveland <torjusti@gmail.com>
2017-08-12 15:48:54 +0200

    Translate wide moves

--- src/utils/cramming.js
+++ src/utils/cramming.js
@@ -10,28 +10,67 @@ const rotations = {
     z: 'FULBDR',
 };
 
+// Wide moves written as a move and a rotation.
+let wideMoves = {
+  'f': 'z B',
+  'r': 'x L',
+  'u': 'y D',
+  'b': "z' F",
+  'l': "x' R",
+  'd': "y' U",
+};
+
+// Translate wide moves into objects as used internally by alg.js.
+Object.keys(wideMoves).forEach((move) => {
+  wideMoves[move] = alg.cube.fromString(wideMoves[move]);
+});
+
 /**
  * Recursively remove all rotations from an algorithm
- ' while preserving the overall effect of the algorithm.
+ * while preserving the overall effect of the algorithm.
+ * Also translates wide moves.
  */
 const stripRotations = (algorithm) => {
   let moves = Array.isArray(algorithm) ? algorithm : alg.cube.fromString(algorithm);
+
+  // Translate all wide moves to moves and a rotation.
+  moves = moves.reduce((acc, move) => {
+    let updated = [];
+
+    if (wideMoves[move.base]) {
+      const amount = move.amount > 0 ? move.amount : 3;
+
+      for (let i = 0; i < amount; i += 1) {
+        updated = updated.concat(wideMoves[move.base]);
+      }
+    } else {
+      updated.push(move);
+    }
+
+    return acc.concat(updated);
+  }, []);
+
   let move = moves.pop();
-  let stack = [];
+
+  let translated = [];
 
   while (move && !rotations[move.base]) {
-    stack.unshift(move);
+    translated.unshift(move);
     move = moves.pop();
   }
 
   if (!move) {
-    return alg.cube.toString(stack);
+    return alg.cube.toString(translated);
   }
 
-  stack = stack.map((obj) => ({
-    ...obj,
-    base: rotations[move.base][moveMap.indexOf(obj.base)],
-  }));
+  for (let i = 0; i < move.amount; i += 1) {
+    translated = translated.map((obj) => ({
+      ...obj,
+      base: rotations[move.base][moveMap.indexOf(obj.base)],
+    }));
+  }
 
-  return stripRotations(moves.concat(stack));
+  // Simplifying will remove duplicate moves which are
+  // caused by the wide move translation.
+  return alg.cube.simplify(stripRotations(moves.concat(translated)));
 };
