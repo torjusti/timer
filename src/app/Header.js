a1dabdcbd1cd0e2c5fe659758195cd24e08b780a
Torjus Iveland <torjusti@gmail.com>
2018-07-01 22:30:14 +0200

    Add scramble selector

--- src/app/Header.js
+++ src/app/Header.js
@@ -8,6 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import ScramblerSelect from 'scrambles/ScramblerSelect';
 import { toggleDrawer } from './actions';
 import { toggleSessionsDialog } from 'sessions/actions';
 
@@ -30,6 +31,8 @@ const Header = ({ showDrawer, showSessionsDialog }) => (
       <Title variant="title" color="inherit">
         Timer
       </Title>
+
+      <ScramblerSelect />
 
       <Button color="inherit" onClick={showSessionsDialog}>Sessions</Button>
     </Toolbar>
