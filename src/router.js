fd5d87cb83b94dc85ef3eed8f01769269e2fcabb
Torjus Iveland <torjusti@gmail.com>
2018-07-02 22:58:37 +0200

    Tear old out results page

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './app/App';
 import SettingsContainer from './settings/SettingsContainer';
-import DetailedResult from 'results/DetailedResult';
 import AlgorithmsContainer from 'algorithms/AlgorithmsContainer';
 import NotFound from 'app/NotFound';
 
@@ -10,7 +9,6 @@ export const routes = {
   home: '/',
   algorithms: '/algorithms',
   settings: '/settings',
-  result: '/result/:id',
 };
 
 const AppRouter = () => (
@@ -19,7 +17,6 @@ const AppRouter = () => (
       <Route exact path={routes.home} component={App} />
       <Route exact path={routes.algorithms} component={AlgorithmsContainer} />
       <Route exact path={routes.settings} component={SettingsContainer} />
-      <Route exact path={routes.result} component={DetailedResult} />
       <Route path="*" component={NotFound} />
     </Switch>
   </BrowserRouter>
