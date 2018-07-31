cbd439a65bba8de665b4fbba1cae1c6b82ed6f0f
Torjus Iveland <torjusti@gmail.com>
2018-07-31 20:53:47 +0200

    Begin configuring progressive web app

--- src/settings/Settings.js
+++ src/settings/Settings.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ExportDataButton from './ExportDataButton';
+import AddProgressiveApp from './AddProgressiveApp';
 
 const Settings = () => (
   <div>
     <h1>Settings</h1>
+
     <h2>Export data</h2>
+
     <ExportDataButton />
+
+    <h2>Progressive web app</h2>
+
+    <AddProgressiveApp />
   </div>
 );
 
