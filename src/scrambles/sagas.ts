1dcb4f1d19bb455a19a06341938ca68aa370bcc1
Torjus Iveland <torjusti@gmail.com>
2019-08-04 21:18:16 +0200

    Do not update scramble when reselecting same scrambler

--- src/scrambles/sagas.ts
+++ src/scrambles/sagas.ts
@@ -1,6 +1,6 @@
 import { takeLatest, call, put, select } from 'redux-saga/effects';
 import { selectedScramblerSelector } from 'sessions/selectors';
-import { SELECT_SCRAMBLER, ADD_RESULT, setScramble, CREATE_SESSION } from 'sessions/actions';
+import { SELECT_SCRAMBLER, ADD_RESULT, setScramble, CREATE_SESSION, SelectScramblerAction } from 'sessions/actions';
 import generateScramble from 'scrambles/generateScramble';
 
 function* updateScramble() {
@@ -10,7 +10,17 @@ function* updateScramble() {
 }
 
 function* scrambleSaga() {
-  yield takeLatest([SELECT_SCRAMBLER, ADD_RESULT, CREATE_SESSION], updateScramble);
+  yield takeLatest([ADD_RESULT, CREATE_SESSION], updateScramble);
+
+  let previousScrambler = yield select(selectedScramblerSelector);
+
+  yield takeLatest(SELECT_SCRAMBLER, function* (action: SelectScramblerAction) {
+    if (action.payload !== previousScrambler) {
+      yield updateScramble();
+    }
+
+    previousScrambler = action.payload;
+  });
 }
 
 export default scrambleSaga;
