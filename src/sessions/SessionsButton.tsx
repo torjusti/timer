351ec8e912b81d0da117279724b850e381391191
Torjus Iveland <torjusti@gmail.com>
2019-08-07 22:54:58 +0200

    Show currently selected session in header

--- src/sessions/SessionsButton.tsx
+++ src/sessions/SessionsButton.tsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 import SessionsOverlay from './SessionsOverlay';
+import { useSelector } from 'react-redux';
+import { selectedSessionSelector } from './selectors';
+
+const StyledButton = styled(Button)`
+  text-transform: none;
+  margin-left: 1rem;
+`;
 
 const SessionsButton: React.FC = () => {
+  const selectedSession = useSelector(selectedSessionSelector);
+
   const [visible, setVisible] = useState<boolean>(false);
 
   return (
     <>
-      <Button color="inherit" onClick={() => setVisible(true)}>
-        Sessions
-      </Button>
+      <StyledButton color="inherit" onClick={() => setVisible(true)}>
+        {selectedSession && selectedSession.name}
+      </StyledButton>
 
       <SessionsOverlay onClose={() => setVisible(false)} open={visible} />
     </>
