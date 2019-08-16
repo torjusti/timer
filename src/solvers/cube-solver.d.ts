9e84c9fff564eb5688e9eb425f3736da0ff68fd8
Torjus Iveland <torjusti@gmail.com>
2019-08-16 17:40:16 +0200

    Improve cube-solver type definitions

--- src/solvers/cube-solver.d.ts
+++ src/solvers/cube-solver.d.ts
@@ -1,3 +1,10 @@
+type Scrmabler = '3x3' | '2gll' | 'cmll' | 'corners' | 'edges' | 'lse' | 'lsll' | 'pll' | 'zbll' | 'zzls';
+
+type Solver = 'kociemba' | 'cross' | 'eoline' | 'fb' | 'xcross';
+
 declare module 'cube-solver' {
-  export const solve: (scramble: string, type: string) => string;
+  // Main method for generating scrambls.
+  export const scramble: (scrambler: Scrambler) => string;
+  // Solve specific subset of a given cube.
+  export const solve: (scramble: string, type: Solver) => string;
 };
